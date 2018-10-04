3ce06f5d185c78af0115942219248be52840220e
Mike Wilkinson <mwilkinson@imo-online.com>
2018-10-03 22:57:07 -0500

    did noauth1 a good

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +9,7 @@ export class LoginComponent implements OnInit {
   constructor() {}
 
   @Output() loggedIn = new EventEmitter<string>();
+  @Input() hasLyingSupport = false;
 
   public users: string[] = [
     'Faustino Boney',
